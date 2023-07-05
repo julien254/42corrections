71ee53c1af08ffbf8488e5eced2a9c301ef442e9
julien254 <juliensfr254@gmail.com>
2023-07-05 12:31:24 +0200

    Correction - test

--- corrections.sh
+++ corrections.sh
@@ -5,14 +5,14 @@ read nameProject
 norm=$(norminette -R CheckForbiddenSourceHeader | grep Error: | wc -l)
 if [ "$norm" -eq 0 ]; then
 	echo "Pas de problemes de Norm !"
-	mkdir "$(nameProject)_corrections"
-	cd "$(nameProject)_corrections/"
+	mkdir "${nameProject}_corrections"
+	cd "${nameProject}_corrections/"
 	git init
 	git config core.sparseCheckout true
 	echo "$nameProject" >> .git/info/sparse-checkout
 	git remote add origin https://github.com/julien254/42corrections.git
 	git pull origin main
-	cd "$(nameProject)/ex00/"
+	cd "${nameProject}/ex00/"
 	make
 else
 	echo "Problemes de NORM !"
