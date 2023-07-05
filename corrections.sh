f3fc53c69a550a87ba1345a1b62852393c729a3f
julien254 <juliensfr254@gmail.com>
2023-07-05 11:58:36 +0200

    Corrections - mise en place du script de correction des projets (phase de test).

--- corrections.sh
+++ corrections.sh
@@ -5,6 +5,13 @@ read nameProject
 norm=$(norminette -R CheckForbiddenSourceHeader | grep Error: | wc -l)
 if [ "$norm" -eq 0 ]; then
 	echo "Pas de problemes de Norm !"
+	mkdir "$nameProject"
+	cd "$nameProject"
+	git init
+	git config core.sparseCheckout true
+	echo "$nameProject" >> .git/info/sparse-checkout
+	git remote add origin https://github.com/julien254/42corrections.git
+	git pull origin main
 	make
 else
 	echo "Problemes de NORM !"
