6b4600be53fb3e54a290bb1215fabc7c75f8b527
julien254 <juliensfr254@gmail.com>
2023-07-06 13:31:36 +0200

    ...

--- c00/c00_corrections.sh
+++ c00/c00_corrections.sh
@@ -24,32 +24,32 @@ echo "\033[1;33;40m        |   ___/  |      /     |  |  |  | .--.  |  | |   __| 
 echo "\033[1;33;40m        |  |      |  |\  \----.|  \`--'  | |  \`--'  | |  |____ |  \`----.    |  |            \033[0m"
 echo "\033[1;33;40m        | _|      | _| \`._____| \______/   \______/  |_______| \______|    |__|   \"c00\"    \033[0m"
 echo "\033[1;33;40m                                                                                           \033[0m"
-sleep 4
+sleep 4;
 
 #____________________WELLCOME______________________________
 
-# delay=0.1
-# echo "\n\n"
-# text="Bienvenue dans le testeur de projet 42!"
+delay=0.1
+echo "\n\n"
+text="Bienvenue dans le testeur de projet 42!"
 
-# i=1
-# while [ "$i" -lt "${#text}" ]; do
-# 	echo -n "$(echo "$text" | cut -c $i)"
-#     sleep "$delay"
-#     i=$(($i + 1))
-# done
+i=1
+while [ "$i" -lt "${#text}" ]; do
+	echo -n "$(echo "$text" | cut -c $i)"
+    sleep "$delay"
+    i=$(($i + 1))
+done
 
-# echo "\n"
-# sleep 1
-# text="Lancement du testeur pour le projet c00"
+echo "\n"
+sleep 1
+text="Lancement du testeur pour le projet c00"
 
-# i=1
-# while [ "$i" -lt "${#text}" ]; do
-# 	echo -n "$(echo "$text" | cut -c $i)"
-#     sleep "$delay"
-#     i=$(($i + 1))
-# done
+i=1
+while [ "$i" -lt "${#text}" ]; do
+	echo -n "$(echo "$text" | cut -c $i)"
+    sleep "$delay"
+    i=$(($i + 1))
+done
 
-# sleep 1;echo -n ".";sleep 1;echo -n ".";sleep 1;echo -n ".";sleep 1
+sleep 1;echo -n ".";sleep 1;echo -n ".";sleep 1;echo -n ".";sleep 1
 
 #_____________________EX00/FT_PUTCHAR_______________________
