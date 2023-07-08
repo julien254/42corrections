25690d8b413a561b245ae3d3a7bb7a866391906a
julien254 <juliensfr254@gmail.com>
2023-07-08 22:01:34 +0200

    ...

--- shell00/shell00_corrections.sh
+++ shell00/shell00_corrections.sh
@@ -6,7 +6,7 @@
 #    By: jdetre <julien.detre.dev@gmail.com>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/07/08 17:31:32 by jdetre            #+#    #+#              #
-#    Updated: 2023/07/08 21:57:31 by jdetre           ###   ########.fr        #
+#    Updated: 2023/07/08 22:01:22 by jdetre           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 #!/bin/sh
@@ -77,9 +77,9 @@ sleep 1;echo -n ".";sleep 1;echo -n ".";sleep 1;echo ".";sleep 1
 
 #_____________________EX00/Z_______________________
 
-result=$(cat -e ../../ex00/z)
+result=$(cat ../../ex00/z)
 
-if [ ${result} = "Z\n" ]; then
+if [ ${result} = $'Z\n' ]; then
 	echo "\nEx00/z : 1$vert$check$resetcolor\n"
 else
 	echo "\nEx00/z : 0rouge$croix$resetcolor\n"
