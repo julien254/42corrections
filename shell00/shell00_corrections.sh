5a965ebb4e0e7d53bd63f84dfe9fa0c90c7711dc
julien254 <juliensfr254@gmail.com>
2023-07-08 19:25:14 +0200

    ...

--- shell00/shell00_corrections.sh
+++ shell00/shell00_corrections.sh
@@ -6,7 +6,7 @@
 #    By: jdetre <julien.detre.dev@gmail.com>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/07/08 17:31:32 by jdetre            #+#    #+#              #
-#    Updated: 2023/07/08 19:20:50 by jdetre           ###   ########.fr        #
+#    Updated: 2023/07/08 19:24:53 by jdetre           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 #!/bin/sh
@@ -74,8 +74,10 @@ sleep 1;echo -n ".";sleep 1;echo -n ".";sleep 1;echo ".";sleep 1
 
 #_____________________EX00/Z_______________________
 
+pwd
 filez="../../ex00/z"
-cat $(filez)
+echo $filez
+cat "$filez"
 resutl=$(cat ../../ex00/z)
 echo "\nEx00/z : $result\n"
 
