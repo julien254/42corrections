9f238a60d5e7c53f9809dcc2f8c5207af88bd27d
julien254 <juliensfr254@gmail.com>
2023-07-09 17:16:58 +0200

    ...

--- shell00/shell00_corrections.sh
+++ shell00/shell00_corrections.sh
@@ -6,7 +6,7 @@
 #    By: jdetre <julien.detre.dev@gmail.com>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/07/08 17:31:32 by jdetre            #+#    #+#              #
-#    Updated: 2023/07/09 17:16:04 by jdetre           ###   ########.fr        #
+#    Updated: 2023/07/09 17:16:55 by jdetre           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 #!/bin/bash
@@ -133,6 +133,6 @@ fi
 #____________________EX04/midLS______________________
 
 midLS=$(ls -tmp)
-result=$(sh ../../ex05/midLS)
+result=$(sh ../../ex04/midLS)
 echo $midLS
 echo $result
