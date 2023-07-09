0402851d9cb85db4eec93b05009448d010407c9a
julien254 <juliensfr254@gmail.com>
2023-07-10 00:14:28 +0200

    ...

--- shell00/shell00_corrections.sh
+++ shell00/shell00_corrections.sh
@@ -6,7 +6,7 @@
 #    By: jdetre <julien.detre.dev@gmail.com>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/07/08 17:31:32 by jdetre            #+#    #+#              #
-#    Updated: 2023/07/10 00:09:38 by jdetre           ###   ########.fr        #
+#    Updated: 2023/07/10 00:14:20 by jdetre           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 #!/bin/bash
@@ -169,8 +169,8 @@ fi
 
 result=$(cat -e ../../ex07/b)
 echo $result
-if [ "${result}" = "Episode V, A NEW H0PE It is a period of civil war\$ Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. \$ During the battle, Rebel spies managed to steal secret plans to the Empire's ultimate weapon, the STAR DEATH, an armored space station with enough power to destroy an entire planet.\$ \$ \$ Pursued by the Empire's sinister agents,\$ Princess Mehdi races home aboard her starship, custodian of the stolen plans that can save her people and restore the dictatorship to the galaxie..\$ \$ \$ \$ \$" ]; then
-	echo "Ex05/diff : $vert$tcheck$resetcolor\n"
-else
-	echo "Ex05/diff : $rouge$croix$resetcolor\n"
-fi
+cat "Episode V, A NEW H0PE It is a period of civil war\$ Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. \$ During the battle, Rebel spies managed to steal secret plans to the Empire's ultimate weapon, the STAR DEATH, an armored space station with enough power to destroy an entire planet.\$ \$ \$ Pursued by the Empire's sinister agents,\$ Princess Mehdi races home aboard her starship, custodian of the stolen plans that can save her people and restore the dictatorship to the galaxie..\$ \$ \$ \$ \$"
+#	echo "Ex05/diff : $vert$tcheck$resetcolor\n"
+#else
+#	echo "Ex05/diff : $rouge$croix$resetcolor\n"
+#fi
