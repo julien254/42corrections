405f2a5759a2639b1ec070f69ee1a39654039d33
julien254 <juliensfr254@gmail.com>
2023-07-09 19:57:05 +0200

    ...

--- shell00/shell00_corrections.sh
+++ shell00/shell00_corrections.sh
@@ -6,7 +6,7 @@
 #    By: jdetre <julien.detre.dev@gmail.com>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/07/08 17:31:32 by jdetre            #+#    #+#              #
-#    Updated: 2023/07/09 19:56:01 by jdetre           ###   ########.fr        #
+#    Updated: 2023/07/09 19:57:03 by jdetre           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 #!/bin/bash
@@ -160,6 +160,7 @@ echo lol > test2.lol
 gitignore=$(git ls-files --others --ignored --exclude-standard)
 result=$(sh ../../ex06/git_ignore.sh)
 echo $gitignore
+echo $result
 if [ "${result}" = "${gitignore}" ]; then
 	echo "Ex06/git_ignore : $vert$tcheck$resetcolor\n"
 else
