fb35b82e6dc1fc5e133af3bce8cda8e66a16702b
julien254 <juliensfr254@gmail.com>
2023-07-09 14:40:15 +0200

    ...

--- shell00/shell00_corrections.sh
+++ shell00/shell00_corrections.sh
@@ -6,7 +6,7 @@
 #    By: jdetre <julien.detre.dev@gmail.com>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/07/08 17:31:32 by jdetre            #+#    #+#              #
-#    Updated: 2023/07/09 14:38:38 by jdetre           ###   ########.fr        #
+#    Updated: 2023/07/09 14:40:09 by jdetre           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 #!/bin/bash
@@ -88,4 +88,5 @@ fi
 #_____________________EX0!/TESTSHELL00___________________
 
 years=$(date +%Y)
-echo $years
+user=$(echo "$USER")
+echo "$years $user"
