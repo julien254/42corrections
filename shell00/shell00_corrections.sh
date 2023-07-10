372c422d3dec35fe507b35a705c25f0d94565b03
Julien Detre <judetre@3A5.42angouleme.fr>
2023-07-10 13:02:28 +0200

    ...

--- shell00/shell00_corrections.sh
+++ shell00/shell00_corrections.sh
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    shell00_corrections.sh                             :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: judetre <julien.detre.dev@gmail.com>        +#+  +:+       +#+         #
+#    By: ${user} <julien.detre.dev@gmail.com>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2023/07/08 17:31:32 by judetre            #+#    #+#              #
-#    Updated: 2023/07/10 12:49:51 by jdetre           ###   ########.fr        #
+#    Created: 2023/07/08 17:31:32 by ${user}            #+#    #+#              #
+#    Updated: 2023/07/10 13:02:20 by jdetre           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 #!/bin/bash
@@ -114,12 +114,12 @@ total=$(ls -l exo2 | xargs | awk '{print $2}')
 octet1=$(ls -l exo2 | xargs | awk '{print $7}')
 octet2=$(ls -l exo2 | xargs | awk '{print $25}')
 result=$(ls -l exo2 | xargs)
-echo "total $total drwx--xr-x 2 judetre ${years}_angouleme $octet1 Jun 1 20:47 test0 -rwx--xr-- 1 judetre ${years}-angouleme 4 Jun 1 21:46 test1 dr-x---r-- 2 judetre ${years}_angouleme $octet2 Jun 1 22:45 test2 -r-----r-- 2 judetre ${years}_angouleme 1 Jun 1 23:44 test3 -rw-r----x 1 judetre ${years}_angouleme 2 Jun 1 23:43 test4 -r-----r-- 2 judetre ${years}_angouleme 1 Jun 1 23:44 test5 lrwxrwxrwx 1 judetre ${years}_angouleme 5 Jun 1 22:20 test6 -> test0"
+echo "total $total drwx--xr-x 2 ${user} ${years}_angouleme $octet1 Jun 1 20:47 test0 -rwx--xr-- 1 ${user} ${years}-angouleme 4 Jun 1 21:46 test1 dr-x---r-- 2 ${user} ${years}_angouleme $octet2 Jun 1 22:45 test2 -r-----r-- 2 ${user} ${years}_angouleme 1 Jun 1 23:44 test3 -rw-r----x 1 ${user} ${years}_angouleme 2 Jun 1 23:43 test4 -r-----r-- 2 ${user} ${years}_angouleme 1 Jun 1 23:44 test5 lrwxrwxrwx 1 ${user} ${years}_angouleme 5 Jun 1 22:20 test6 -> test0"
 echo $result
-if [ "${result}" = "total $total drwx--xr-x 2 judetre ${years}_angouleme $octet1 Jun 1 20:47 test0 -rwx--xr-- 1 judetre ${years}-angouleme 4 Jun 1 21:46 test1 dr-x---r-- 2 judetre ${years}_angouleme $octet2 Jun 1 22:45 test2 -r-----r-- 2 judetre ${years}_angouleme 1 Jun 1 23:44 test3 -rw-r----x 1 judetre ${years}_angouleme 2 Jun 1 23:43 test4 -r-----r-- 2 judetre ${years}_angouleme 1 Jun 1 23:44 test5 lrwxrwxrwx 1 judetre ${years}_angouleme 5 Jun 1 22:20 test6 -> test0" ]; then
+if [ "${result}" = "total $total drwx--xr-x 2 ${user} ${years}_angouleme $octet1 Jun 1 20:47 test0 -rwx--xr-- 1 ${user} ${years}-angouleme 4 Jun 1 21:46 test1 dr-x---r-- 2 ${user} ${years}_angouleme $octet2 Jun 1 22:45 test2 -r-----r-- 2 ${user} ${years}_angouleme 1 Jun 1 23:44 test3 -rw-r----x 1 ${user} ${years}_angouleme 2 Jun 1 23:43 test4 -r-----r-- 2 ${user} ${years}_angouleme 1 Jun 1 23:44 test5 lrwxrwxrwx 1 ${user} ${years}_angouleme 5 Jun 1 22:20 test6 -> test0" ]; then
 	echo "Ex02/exo2.tar : $vert$tcheck$resetcolor\n"
 	point=$((point + 1))
-elif [ "${result}" = "total 24 drwx--xr-x 2 judetre ${years}_angouleme 4096 Jun 1 ${years} test0 -rwx--xr-- 1 judetre ${years}_angouleme 4 Jun 1 ${years} test1 dr-x---r-- 2 judetre ${years}_angouleme 4096 Jun 1 ${years} test2 -r-----r-- 2 judetre ${years}_angouleme 1 Jun 1 ${years} test3 -rw-r----x 1 judetre ${years}_angouleme 2 Jun 1 ${years} test4 -r-----r-- 2 judetre ${years}_angouleme 1 Jun 1 ${years} test5 lrwxrwxrwx 1 judetre ${years}_angouleme 5 Jun 1 ${years} test6 -> test0" ]; then
+elif [ "${result}" = "total 24 drwx--xr-x 2 ${user} ${years}_angouleme 4096 Jun 1 ${years} test0 -rwx--xr-- 1 ${user} ${years}_angouleme 4 Jun 1 ${years} test1 dr-x---r-- 2 ${user} ${years}_angouleme 4096 Jun 1 ${years} test2 -r-----r-- 2 ${user} ${years}_angouleme 1 Jun 1 ${years} test3 -rw-r----x 1 ${user} ${years}_angouleme 2 Jun 1 ${years} test4 -r-----r-- 2 ${user} ${years}_angouleme 1 Jun 1 ${years} test5 lrwxrwxrwx 1 ${user} ${years}_angouleme 5 Jun 1 ${years} test6 -> test0" ]; then
 	echo "Ex02/exo2.tar : $vert$tcheck$resetcolor\n"
 	point=$((point + 1))
 else
