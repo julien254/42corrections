54dcb1921e5f39ef513a380e4c5bf623569be7e7
julien254 <juliensfr254@gmail.com>
2023-07-10 02:19:51 +0200

    ...

--- shell00/shell00_corrections.sh
+++ shell00/shell00_corrections.sh
@@ -6,7 +6,7 @@
 #    By: jdetre <julien.detre.dev@gmail.com>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/07/08 17:31:32 by jdetre            #+#    #+#              #
-#    Updated: 2023/07/10 02:18:26 by jdetre           ###   ########.fr        #
+#    Updated: 2023/07/10 02:19:47 by jdetre           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 #!/bin/bash
@@ -179,7 +179,7 @@ fi
 liste=$(find . -type f \( -name '*~' -o -name '#*#' \) -print)
 result=$(sh ../../ex08/clean)
 if [ "${result}" = "${liste}" ] && [ $(ls -l | grep -e test8 -e test9 | wc -l) -eq 0 ] && [ $(ls -l | grep test10 | wc -l) -eq 1 ] && [ $(ls -l test10 | grep -e test8 -e test9 | wc -l) -eq 0 ]; then
-	echo "Ex05/git_commit : $vert$tcheck$resetcolor\n"
+	echo "Ex08/git_commit : $vert$tcheck$resetcolor\n"
 else
-	echo "Ex05/git_commit : $rouge$croix$resetcolor\n"
+	echo "Ex08/git_commit : $rouge$croix$resetcolor\n"
 fi
